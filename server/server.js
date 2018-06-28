56c88b456f8b991ed792c142e8a8dfd5b0bcbc95
Shreyas B1 <shreyas.b1@olacabs.com>
2018-06-29 00:52:30 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path=require('path');
+const express=require('express');
+const app=express();
+const publicPath=path.join(__dirname,'../public');
+
+app.use(express.static(publicPath));
+const port=process.env.port||5000;
+app.listen(port,(err,res)=>{
+  console.log(`Server Started ${port}`);
+});
